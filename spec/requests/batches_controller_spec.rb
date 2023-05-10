3083b4bf3bb35eb458b1572623d5dcb134ef68c4
Anil Yanduri <anilkumaryln@gmail.com>
2023-05-10 15:13:13 +0530

    added specs

--- spec/requests/batches_controller_spec.rb
+++ spec/requests/batches_controller_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "BatchesControllers", type: :request do
   let (:student) { user.add_role(:student, school); user }
   let (:course) { create(:course, school_id: school.id) }
   let (:batch) { create(:batch, course_id: course.id, school_id: school.id) }
+  let (:enrollment) { build(:enrollment, batch_id: batch.id, user_id: student.id, status: 1)}
 
   describe "GET /index" do
     it "it should be 200 for school admin" do
@@ -40,6 +41,50 @@ RSpec.describe "BatchesControllers", type: :request do
       school
       put "/admin/batches/#{batch.id}", params: {"batch"=>{"name"=>"batch 1 for course 1 test update", "status"=>"1", "course_id"=>course.id}}
       expect(batch.reload.name).to eq "batch 1 for course 1 test update"
+    end
+  end
+
+  describe "Enroll as a student" do
+    it "schold let student enroll" do
+      login(student)
+      expect {
+        put "/batches/#{batch.id}/enroll", params: {}, xhr: true
+      }.to change(Enrollment, :count).by(1)
+    end
+
+    it "schold not let student enroll" do
+      login(student)
+      enrollment.student = student
+      enrollment.batch_id = batch.id
+      enrollment.status = 1
+      enrollment.save
+      expect {
+        put "/batches/#{batch.id}/enroll", params: {}, xhr: true
+      }.not_to change(Enrollment, :count)
+    end
+  end
+
+  describe "endorse enrolments" do
+    it "schold let admin endorse approve" do
+      login(school_admin)
+      enrollment.student = student
+      enrollment.batch_id = batch.id
+      enrollment.status = 0
+      enrollment.save
+
+      put "/admin/batches/#{batch.id}/endorse/approve", params: {"student_id"=>student.id}, xhr: true
+      expect(enrollment.reload.status).to eq('approved')
+    end
+
+    it "schold let admin endorse approve" do
+      login(school_admin)
+      enrollment.student = student
+      enrollment.batch_id = batch.id
+      enrollment.status = 0
+      enrollment.save
+
+      put "/admin/batches/#{batch.id}/endorse/reject", params: {"student_id"=>student.id}, xhr: true
+      expect(enrollment.reload.status).to eq('rejected')
     end
   end
 end
